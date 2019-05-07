d025502e8881a09c95241366a6b8d3d2bb23229f
Nisha <nishishendkar288@gmail.com>
2019-05-07 01:20:33 -0700

    Finished styling

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,16 +1,36 @@
+
+
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+   rootStyles.getPropertyValue('--book-cover-width-large') != ''){
+    ready()
+} else{
+    document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready(){
+
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
+
+    const coverHeight = coverWidth / coverAspectRatio
+
+    FilePond.registerPlugin(
+        FilePondPluginFileEncode,
+        FilePondPluginImageResize,
+        FilePondPluginImagePreview
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    // Turn all file input elements into ponds
+    
+    FilePond.parse(document.body);
+}
+
 //Register all the Filepond plugins which are to be used in the project
 
-FilePond.registerPlugin(
-    FilePondPluginFileEncode,
-    FilePondPluginImageResize,
-    FilePondPluginImagePreview
-)
-
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
-// Turn all file input elements into ponds
-
-FilePond.parse(document.body);
